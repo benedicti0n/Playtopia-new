347a92d785652df5b0101f73d85b0d4cfe5ee7e3
subhraneel2005 <digitalsubhraneel@gmail.com>
2024-03-09 22:58:46 +0530

    Carousel Added

---
+++ Frontend/Playtopia/src/components/games.js
@@ -0,0 +1,54 @@
+import bg1 from "/EventBg/Desktop/bgImg1.jpg";
+import bg2 from "/EventBg/Desktop/bgImg2.jpg";
+import bg3 from "/EventBg/Desktop/bgImg3.jpg";
+import bg4 from "/EventBg/Desktop/bgImg4.jpg";
+import bg5 from "/EventBg/Desktop/bgImg5.jpg";
+import bg6 from "/EventBg/Desktop/bgImg6.jpg";
+import bg7 from "/EventBg/Desktop/bgImg7.jpg";
+
+const games = [
+    {
+        id: 1,
+        title: "The Last of Us",
+        platform: "PlayStation 3",
+        src: bg1,
+    },
+    {
+        id: 2,
+        title: "The Last of Us",
+        platform: "PlayStation 3",
+        src: bg2,
+    },
+    {
+        id: 3,
+        title: "The Last of Us",
+        platform: "PlayStation 3",
+        src: bg3,
+    },
+    {
+        id: 4,
+        title: "The Last of Us",
+        platform: "PlayStation 3",
+        src: bg4,
+    },
+    {
+        id: 5,
+        title: "The Last of Us",
+        platform: "PlayStation 3",
+        src: bg5,
+    },
+    {
+        id: 6,
+        title: "The Last of Us",
+        platform: "PlayStation 3",
+        src: bg6,
+    },
+    {
+        id: 7,
+        title: "The Last of Us",
+        platform: "PlayStation 3",
+        src: bg7,
+    },
+]
+
+export default games;
