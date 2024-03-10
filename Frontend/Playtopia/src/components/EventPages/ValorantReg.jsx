6eb789eed131c2687048a5aedb341777d93e31df
Ashesh Bandopadhyay <lbandopadhyay2@gmail.com>
2024-03-10 14:51:37 +0530

    valo reg form

--- Frontend/Playtopia/src/components/EventPages/ValorantReg.jsx
+++ Frontend/Playtopia/src/components/EventPages/ValorantReg.jsx
@@ -2,69 +2,86 @@ import React from "react"
 
 const ValorantReg = () => {
     return (
-        <section className="h-screen w-full bg-white">
+        <section className="h-screen w-full bg-gray-800">
             <div className="flex justify-center h-full w-full">
-                <div className="hidden bg-cover bg-center lg:block lg:w-2/5" style={{ backgroundImage: 'url("/bgmi.jpg")' }}>
+                <div className="hidden bg-cover bg-top lg:block lg:w-2/5" style={{ backgroundImage: 'url("/bgMobile.png")' }}>
                 </div>
                 <div className="flex items-center w-full max-w-3xl p-8 mx-auto lg:px-12 lg:w-3/5">
-                    <div className="w-full">
-                        <h1 className="text-3xl font-bold tracking-wider text-gray-800 capitalize ">
-                            Register with BGMI
+                    <div className="w-full ">
+                        <h1 className="text-3xl font-bold tracking-wider text-white capitalize text-center">
+                            Register with Valorant
                         </h1>
-                        <form className="grid grid-cols-1 gap-5 mt-8 md:grid-cols-2">
+                        <form className="grid grid-cols-2 gap-5 mt-8 md:grid-cols-2 lg:grid-cols-4">
                             <div>
-                                <label className="block mb-2 text-sm text-gray-600">Full Name</label>
-                                <input type="text" placeholder="John" className="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-lg focus:border-blue-400 focus:outline-none focus:ring focus:ring-blue-400 focus:ring-opacity-40" />
+                                <label className="block mb-2 text-sm text-gray-600">Team Leader Name</label>
+                                <input type="text" placeholder="" className="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-lg focus:border-blue-400 focus:outline-none focus:ring focus:ring-blue-400 focus:ring-opacity-40" />
                             </div>
                             <div>
-                                <label className="block mb-2 text-sm text-gray-600">Phone number</label>
-                                <input type="text" placeholder="XXX-XX-XXXX-XXX" className="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-lg focus:border-blue-400 focus:outline-none focus:ring focus:ring-blue-400 focus:ring-opacity-40" />
+                                <label className="block mb-2 text-sm text-gray-600">Whatsapp number</label>
+                                <input type="text" placeholder="" className="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-lg focus:border-blue-400 focus:outline-none focus:ring focus:ring-blue-400 focus:ring-opacity-40" />
                             </div>
                             <div>
                                 <label className="block mb-2 text-sm text-gray-600">Email address</label>
-                                <input type="email" placeholder="johnsnow@example.com" className="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-lg focus:border-blue-400 focus:outline-none focus:ring focus:ring-blue-400 focus:ring-opacity-40" />
+                                <input type="email" placeholder="" className="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-lg focus:border-blue-400 focus:outline-none focus:ring focus:ring-blue-400 focus:ring-opacity-40" />
                             </div>
                             <div>
-                                <label className="block mb-2 text-sm text-gray-600">Team Name <span className="font-bold">Optional</span></label>
-                                <input type="text" placeholder="team name" className="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-lg focus:border-blue-400 focus:outline-none focus:ring focus:ring-blue-400 focus:ring-opacity-40" />
+                                <label className="block mb-2 text-sm text-gray-600">Team Name </label>
+                                <input type="text" placeholder="" className="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-lg focus:border-blue-400 focus:outline-none focus:ring focus:ring-blue-400 focus:ring-opacity-40" />
                             </div>
                             <div>
-                                <label className="block mb-2 text-sm text-gray-600">Select 1 event form:</label>
-                                <div className="flex items-center gap-5">
-                                    <div className="flex items-center mt-2">
-                                        <input type="radio" id="bgmi" name="game" value="BGMI" className="hidden" />
-                                        <label htmlFor="bgmi" className="text-sm bg-gray-300 hover:bg-gray-400 text-gray-700 rounded-lg px-4 py-2 cursor-pointer">BGMI</label>
-                                    </div>
-                                    <div className="flex items-center mt-2">
-                                        <input type="radio" id="pes" name="game" value="PES" className="hidden" />
-                                        <label htmlFor="pes" className="text-sm bg-gray-300 hover:bg-gray-400 text-gray-700 rounded-lg px-4 py-2 cursor-pointer">PES</label>
-                                    </div>
-                                    <div className="flex items-center mt-2">
-                                        <input type="radio" id="valorant" name="game" value="VALORANT" className="hidden" />
-                                        <label htmlFor="valorant" className="text-sm bg-gray-300 hover:bg-gray-400 text-gray-700 rounded-lg px-4 py-2 cursor-pointer">VALORANT</label>
-                                    </div>
-                                </div>
+                                <label className="block mb-2 text-sm text-gray-600">Player 1 Name</label>
+                                <input type="text" placeholder="" className="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-lg focus:border-blue-400 focus:outline-none focus:ring focus:ring-blue-400 focus:ring-opacity-40" />
                             </div>
                             <div>
-                                <label className="block mb-2 text-sm text-gray-600">Select 1 game if u want to participate</label>
-                                <div className="flex items-center gap-5">
-                                    <div className="flex items-center mt-2">
-                                        <input type="radio" id="bgmi" name="game" value="BGMI" className="hidden" />
-                                        <label htmlFor="bgmi" className="text-sm bg-gray-300 hover:bg-gray-400 text-gray-700 rounded-lg px-4 py-2 cursor-pointer">NFS</label>
-                                    </div>
-                                    <div className="flex items-center mt-2">
-                                        <input type="radio" id="pes" name="game" value="PES" className="hidden" />
-                                        <label htmlFor="pes" className="text-sm bg-gray-300 hover:bg-gray-400 text-gray-700 rounded-lg px-4 py-2 cursor-pointer">8 BPool</label>
-                                    </div>
-                                    <div className="flex items-center mt-2">
-                                        <input type="radio" id="valorant" name="game" value="VALORANT" className="hidden" />
-                                        <label htmlFor="valorant" className="text-sm bg-gray-300 hover:bg-gray-400 text-gray-700 rounded-lg px-4 py-2 cursor-pointer">Carom</label>
-                                    </div>
-                                </div>
+                                <label className="block mb-2 text-sm text-gray-600">Player 1 Riot ID</label>
+                                <input type="text" placeholder="" className="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-lg focus:border-blue-400 focus:outline-none focus:ring focus:ring-blue-400 focus:ring-opacity-40" />
                             </div>
+                            <div>
+                                <label className="block mb-2 text-sm text-gray-600">Player 2 Name</label>
+                                <input type="text" placeholder="" className="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-lg focus:border-blue-400 focus:outline-none focus:ring focus:ring-blue-400 focus:ring-opacity-40" />
+                            </div>
+                            <div>
+                                <label className="block mb-2 text-sm text-gray-600">Player 2 Riot ID</label>
+                                <input type="text" placeholder="" className="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-lg focus:border-blue-400 focus:outline-none focus:ring focus:ring-blue-400 focus:ring-opacity-40" />
+                            </div>
+                            <div>
+                                <label className="block mb-2 text-sm text-gray-600">Player 3 Name</label>
+                                <input type="text" placeholder="" className="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-lg focus:border-blue-400 focus:outline-none focus:ring focus:ring-blue-400 focus:ring-opacity-40" />
+                            </div>
+                            <div>
+                                <label className="block mb-2 text-sm text-gray-600">Player 3 Riot ID</label>
+                                <input type="text" placeholder="" className="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-lg focus:border-blue-400 focus:outline-none focus:ring focus:ring-blue-400 focus:ring-opacity-40" />
+                            </div>
+                            <div>
+                                <label className="block mb-2 text-sm text-gray-600">Player 4 Name</label>
+                                <input type="text" placeholder="" className="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-lg focus:border-blue-400 focus:outline-none focus:ring focus:ring-blue-400 focus:ring-opacity-40" />
+                            </div>
+                            <div>
+                                <label className="block mb-2 text-sm text-gray-600">Player 4 Riot ID</label>
+                                <input type="text" placeholder="" className="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-lg focus:border-blue-400 focus:outline-none focus:ring focus:ring-blue-400 focus:ring-opacity-40" />
+                            </div>
+                            <div>
+                                <label className="block mb-2 text-sm text-gray-600">Player 5 Name</label>
+                                <input type="text" placeholder="" className="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-lg focus:border-blue-400 focus:outline-none focus:ring focus:ring-blue-400 focus:ring-opacity-40" />
+                            </div>
+                            <div>
+                                <label className="block mb-2 text-sm text-gray-600">Player 5 Riot ID</label>
+                                <input type="text" placeholder="" className="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-lg focus:border-blue-400 focus:outline-none focus:ring focus:ring-blue-400 focus:ring-opacity-40" />
+                            </div>
+                            <div>
+                                <label className="block mb-2 text-sm text-gray-600">Player 6 Name</label>
+                                <input type="text" placeholder="" className="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-lg focus:border-blue-400 focus:outline-none focus:ring focus:ring-blue-400 focus:ring-opacity-40" />
+                            </div>
+                            <div>
+                                <label className="block mb-2 text-sm text-gray-600">Player 6 Riot ID</label>
+                                <input type="text" placeholder="" className="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-lg focus:border-blue-400 focus:outline-none focus:ring focus:ring-blue-400 focus:ring-opacity-40" />
+                            </div>
+                            
+                            <div className="w-full grid grid-cols-none">
                             <button className="flex items-center justify-center w-full px-5 py-3 mt-4 text-sm tracking-wide text-white capitalize transition-colors duration-300 transform bg-[#ff4655] rounded-lg hover:bg-indigo-400 focus:outline-none focus:ring focus:ring-blue-400 focus:ring-opacity-50">
                                 <span className=" font-semibold">Register Now</span>
-                            </button>
+                            </button>ß
+                            </div>
                         </form>
                     </div>
                 </div>
